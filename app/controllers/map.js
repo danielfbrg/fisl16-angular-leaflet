7f0edde594b5d6d8327f22eaf097f39f5067ab61
dagnaldo <dagnaldo.silva@hexgis.com>
2015-07-02 20:02:22 -0300

    Creating directive

--- app/controllers/map.js
+++ app/controllers/map.js
@@ -1,18 +1,10 @@
 var app = angular.module('myApp', [])
 
-.controller('MapController', ['$scope', function($scope) {
+.controller('MapController', function($scope, $http) {
 
     $scope.changePoint = function() {
-        $scope.marker.setLatLng( [$scope.lat, $scope.lng] );
+        $scope.marker.setLatLng( [$scope.lat, $scope.lng]);
     };
 
-    var map = L.map('map').setView([-30.034102019390673, -51.2052583694458], 13);
 
-    L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-        attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-    }).addTo(map);
-
-    $scope.marker = L.marker([-30.0577843,-51.1732458]).addTo(map)
-    .bindPopup('<strong>Evento:</strong> FISL 16. <br> <strong>Local:</strong> PUCRS')
-    .openPopup();
-}]);
+});
