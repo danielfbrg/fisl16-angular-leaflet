8aff551cb9663376dcdcb684587c950e6dbcb614
Eder Carlos <eder@innovateti.com.br>
2015-07-06 13:20:04 -0300

    Adding http request to load a GeoJSON layer

--- app/controllers/map.js
+++ app/controllers/map.js
@@ -6,5 +6,16 @@ var app = angular.module('myApp', [])
         $scope.marker.setLatLng( [$scope.lat, $scope.lng]);
     };
 
+    $http.get('assets/geojson/pubs.json').
+    success(function(data, event){
+        L.geoJson(data, {
+            onEachFeature: function (feature, layer) {
+                layer.bindPopup(feature.properties.name);
+        }
+    }).addTo($scope.map);
 
+    }).
+    error(function(data, event){
+
+    })
 });
--- app/directives/map.js
+++ app/directives/map.js
@@ -2,15 +2,15 @@ angular.module('myApp')
 .directive('map', function(){
 	return{
 		template: '<div id="map"></div>', 
-		restrict: 'AE',
+		restrict: 'E',
 		controller: function($scope){
-		   var map = L.map('map').setView([-30.034102019390673, -51.2052583694458], 13);
+		   $scope.map = L.map('map').setView([-30.034102019390673, -51.2052583694458], 13);
 
 		    L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
 		        attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-		    }).addTo(map);
+		    }).addTo($scope.map);
 
-		    $scope.marker = L.marker([-30.0577843,-51.1732458]).addTo(map)
+		    $scope.marker = L.marker([-30.0577843,-51.1732458]).addTo($scope.map)
 		    .bindPopup('<strong>Evento:</strong> FISL 16. <br> <strong>Local:</strong> PUCRS')
 		    .openPopup();
 		}
