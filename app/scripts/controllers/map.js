66bf1032f5cb2107bfd6939e5af5f2536547a6a6
Eder Carlos <eder@innovateti.com.br>
2015-07-08 02:46:35 -0300

    Adding thematic map of Brazil

--- app/scripts/controllers/map.js
+++ app/scripts/controllers/map.js
@@ -2,21 +2,41 @@ var app = angular.module('myApp', [])
 
 .controller('MapController', function($scope, $http) {
 
-    $scope.changePoint = function() {
-        $scope.marker.setLatLng( [$scope.lat, $scope.lng]);
-    };
+    $http.get('assets/geojson/brazil-states-simplified.geojson').
+    success(function(data, event){
+        var layer = L.geoJson(data, {
+            style: style,
+            onEachFeature: function (feature, layer) {
+                layer.bindPopup(feature.properties.sigla);
+        }
+    }).addTo($scope.map);
 
-    // $http.get('assets/geojson/pubs.json').
-    // success(function(data, event){
-    //     L.geoJson(data, {
-    //         onEachFeature: function (feature, layer) {
-    //             layer.bindPopup(feature.properties.name);
-    //     }
-    // }).addTo($scope.map);
+    $scope.map.fitBounds(layer.getBounds());
 
-    // }).
-    // error(function(data, event){
+    }).
+    error(function(data, event){
+        console.log('Error getting json');
+    })
 
-    // })
-  
+
+    function getColor(regiao) {
+        return regiao == 'NORDESTE'     ? '#FC4E2A' :
+               regiao == 'NORTE'        ? '#009933' :
+               regiao == 'SUDESTE'      ? '#E31A1C' :
+               regiao == 'SUL'          ? '#0066FF' :
+               regiao == 'CENTRO-OESTE' ? '#FFCC00' :
+                                          '#FFEDA0';
+    }
+
+    function style(feature) {
+        return {
+            fillColor: getColor(feature.properties.regiao),
+            weight: 2,
+            opacity: 1,
+            color: 'white',
+            dashArray: '3',
+            fillOpacity: 0.7
+        };
+    }
+
 });
--- app/scripts/directives/map.js
+++ app/scripts/directives/map.js
@@ -3,31 +3,20 @@ angular.module('myApp')
 	return{
 		template: '<div id="map"></div>', 
 		restrict: 'E',
-		controller: function($scope, $http){
+		controller: function($scope){
 		    $scope.map = L.map('map').setView([-30.034102019390673, -51.2052583694458], 13);
 
 		    L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
 		        attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 		    }).addTo($scope.map);
 
-		    $scope.marker = L.marker([-30.0577843,-51.1732458]).addTo($scope.map)
-		    .bindPopup('<strong>Evento:</strong> FISL 16. <br> <strong>Local:</strong> PUCRS')
-		    // .openPopup();
+		    var greenIcon = L.icon({
+			    iconUrl: 'assets/img/fisl_icon.png',
+			    iconSize: [38, 38],
+			});
 
-	      	$http.get('assets/geojson/brazil-states-simplified.geojson').
-		    success(function(data, event){
-		        var layer = L.geoJson(data, {
-		            onEachFeature: function (feature, layer) {
-		                layer.bindPopup(feature.properties.sigla);
-		        }
-	    	}).addTo($scope.map);
-
-		    	$scope.map.fitBounds(layer.getBounds());
-
-		    }).
-		    error(function(data, event){
-		    	console.log('Error getting json');
-		    })
+	        var marker = L.marker([-30.0577843,-51.1732458], {icon: greenIcon}).addTo($scope.map)
+  	 	     .bindPopup('<strong>Evento:</strong> FISL 16. <br> <strong>Local:</strong> PUCRS')
 		}
 	}
 })
